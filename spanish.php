93f261326440a214623293861be2c4cc6b966b55
EztebanD <lucho8891@hotmail.com>
2014-11-12 15:09:22 -0500

    Create spanish.php

---
+++ spanish.php
@@ -0,0 +1,53 @@
+<?php
+$_lang["Download"] = "Descargar";
+$_lang["Home"] = "Inicio";
+$_lang["Status"] = "Estado";
+$_lang["Username"] = "Usuario";
+$_lang["Password"] = "Contraseña";
+$_lang["Login"] = "Iniciar Sesion";
+$_lang["Register"] = "Registro";
+$_lang["Download latest version"] = "Descargue la version más reciente";
+$_lang["Current release:"] = "Version actual:";
+$_lang["Language:"] = "Lenguaje:";
+$_lang["JOIN"] = "Unirse";
+$_lang["Join thousands of truck enthusiasts on-line!"] = "Unase a miles de entusiastas en linea!";
+$_lang["Voice interaction with other drivers - click to join TS3 server."] = "Interaccion de voz con otros conductores - Haga 
+clic para unirse al servidor TS3.";
+$_lang["Latest news on our facebook."] = "Ultimas noticias en nuestro Facebook.";
+$_lang["registration.blocked"] = "El registro esta bloqueado - todavia estamos trabajando en algunas caracteristicas que nos 
+obligo a bloquear el registro. Lo desbloquearemos cuando hagamos todo y publicaremos una nueva version";
+$_lang["invalid.username.password"] = "Usuario o contraseña invalidos";
+$_lang["already.logged"] = "Usted ya esta logeado";
+$_lang["welcome"] = "Bienvenido";
+$_lang["logout"] = "Cerrar Sesion";
+$_lang["bans"] = "Prohibidos";
+$_lang["download"] = "Descargar";
+$_lang["version"] = "Version";
+$_lang["update.time"] = "Hora de Actualizacion";
+$_lang["req.game.version"] = "Version del juego requerida";
+$_lang["requires"] = "Requiere";
+$_lang["or.newer"] = "o más reciente";
+$_lang["game.client"] = "Cliente del juego";
+$_lang["download.no.permissions"] = "Para descargar el mod, tiene que estar conectado";
+$_lang["why.create.account"] = "¿Por qué crear una cuenta? <br />Una cuenta se utilizara para acceder al sistema para todos los 
+servicios (foros, juegos, economia y otros). Tenemos la intencion de utilizarlos para introducir la economia y las estadisticas 
+del conductor personal en el futuro. Usted podra modificar su propio perfil";
+$_lang["website.title"] = "ETS2MP - Mundo virtual de Camiones - mod Multijugador para el juego Euro Truck Simulator 2 de SCS 
+Software";
+$_lang["cookies.warning"] = "Este sitio uriliza cookies de <a href=\"http://en.wikipedia.org/wiki/HTTP_cookie\">cookies</a>. Al 
+continuar navegando por el sitio, usted acepta el uso de cookies.";
+$_lang["forum"] = "Foro";
+$_lang["You cannot create new account while you are logged."] = "No puede crear una nueva cuenta mientras esta conectado.";
+$_lang["You've been successfully registered."] = "Usted ha sido registrado con exito.";
+$_lang["Unknown error"] = "Error desconocido";
+$_lang["Username, mail or steamid is already taken."] = "Nombre de usuario, correo o steamid ya esta en uso.";
+$_lang["E-Mail"] = "Correo";
+$_lang["To create ETS2MP account you must have ETS2 assigned to your steam account. If you have it in your steam library try to 
+check if your profile is public. If it's private make it public and press"] = "Para crear una cuenta ETS2MP debe tener ETS2 
+asignado a su cuenta de Steam. Si lo tienes en tu biblioteca de Steam tratar de comprobar si su perfil es publico, Si es provado 
+hacerlo publico y presiona";
+$_lang["To register you have to login to your steam account. Press button bellow to redirect to steam authentication website."] 
+= "Para registrarse hay que acceder a su cuenta de Steam. Presione el boton de abajo para redirijir a la pagina de 
+Autentificacion de Steam.";
+$_lang["rules.info"] = "Al pulsar el boton registrarse usted acepta nuestras reglas de la comunidad. Para comprobarlas haga clic 
+aquí";
